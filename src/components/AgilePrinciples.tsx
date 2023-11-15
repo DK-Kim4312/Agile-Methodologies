9f6f1f76b5d1edb6b3716f695752f99df3eb508a
DK Kim <dkim693@wisc.edu>
2023-11-15 16:40:08 -0600

    init commit

---
+++ src/components/AgilePrinciples.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AgilePrinciples: React.FC = () => {
+  return (
+    <div>
+      <h2>Agile Principles</h2>
+      {/* List and explain principles */}
+    </div>
+  );
+}
+
+export default AgilePrinciples;
