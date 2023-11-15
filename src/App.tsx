31bda942fcc189994e9338958b500b3e10287bec
DK Kim <dkim693@wisc.edu>
2023-11-15 17:07:21 -0600

    Added Contents as well as navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import AgilePrinciples from './components/AgilePrinciples';
 // other imports
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/agile-principles" element={<AgilePrinciples />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agile-principles" element={<AgilePrinciples />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; // Assuming you have a CSS file for the Home component
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to Agile Methodologies</h1>
-      {/* Content */}
+    <div className="home-container">
+      <h1>Welcome to Agile Development Methodologies</h1>
+      <p>Explore the core principles and practices of Agile Development.</p>
+      
+      {/* Navigation Link to AgilePrinciples Page */}
+      <Link to="/agile-principles" className="navigate-button">
+        Learn about Agile Principles
+      </Link>
     </div>
   );
 }
